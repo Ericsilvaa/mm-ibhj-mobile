0797132f6ad99ebbebf6a795cfe2db97a502a46f
Eric <eriicsilva28@gmail.com.br>
2024-06-16 20:18:05 -0300

    start app

---
+++ app/(home)/(tabs)/all-musics.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const AllMusics = () => {
+  return (
+    <View>
+      <Text>all-musics</Text>
+    </View>
+  );
+};
+
+export default AllMusics;
+
+const styles = StyleSheet.create({});
---
+++ app/(home)/(tabs)/liturgy.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const liturgy = () => {
+  return (
+    <View>
+      <Text>liturgy</Text>
+    </View>
+  );
+};
+
+export default liturgy;
+
+const styles = StyleSheet.create({});
